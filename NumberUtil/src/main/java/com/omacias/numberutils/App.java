16c8fd1eb9a0adde60528336dbcf5170cc7b922b
Macias, Salvador (Java Dev) <salvador.macias.91@gmail.com>
2015-11-27 10:12:29 -0600

    creating maven project and updating gitignore file

---
+++ NumberUtil/src/main/java/com/omacias/numberutils/App.java
@@ -0,0 +1,13 @@
+package com.omacias.numberutils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
